a42e8dd30cdc843b317026c73f1bca39d07119f9
vladyslav223 <vladyslav223@gmail.com>
2019-12-08 16:35:08 +0200

    Updates

---
+++ precache-manifest.6acaf71d73537c4d7b988cf93fc99c5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d47869570ac549794fbeb63d340736",
+    "url": "/shark-react/index.html"
+  },
+  {
+    "revision": "1d6dc6e3f7885596ab0a",
+    "url": "/shark-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "50d52f40bdf59bcffb65",
+    "url": "/shark-react/static/js/2.a8fbb137.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shark-react/static/js/2.a8fbb137.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d6dc6e3f7885596ab0a",
+    "url": "/shark-react/static/js/main.4e459e7a.chunk.js"
+  },
+  {
+    "revision": "ae15e5bdc6e53a3b233f",
+    "url": "/shark-react/static/js/runtime-main.e12406e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shark-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
